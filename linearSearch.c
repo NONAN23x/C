c405601dc58da8de0d6daaf64fac5c36c30cb3d5
Nomaan JamalUddin <61901753+NONAN23x@users.noreply.github.com>
2022-07-10 19:19:42 +0530

    Add files via upload

---
+++ linearSearch.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int linearSearch(int target, int n, int array[])
+{
+    for (int i=0; i <= n-1 ; i++)
+    {
+        if (array[i] == target)
+        {
+            return 0;
+            break;
+        }
+        else
+        {
+            return 1;
+            break;
+        }
+    }
+
+}
+
+int main()
+{
+    int target = 1337;
+    int n = 0;
+    int result;
+    int array[] = {10, 5, 6, 1, 2, 7, 4, 8, 9, 1337, 22, 80};
+    n = sizeof(array)/sizeof(array[0]);
+
+    result = linearSearch(target, n, array);
+    
+    if (result)
+        printf("Element %d found inside the given array\n", target);
+    else
+        printf("Element %d not found.\n", target);
+
+    return 0;
+}
