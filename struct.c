0020a834fc60445ebec8823c301700a9b7b96c77
Nomaan JamalUddin <61901753+NONAN23x@users.noreply.github.com>
2022-10-15 07:48:56 +0530

    Update struct.c

--- struct.c
+++ struct.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
+#include <string.h>
 
 struct item
 {
-    int *itemName;
+    char itemName[50];
     int quantity;
     float price;
-    float amount;
-};
+}gamingConsole;
 
-
-int readItem()
+int main()
 {
-
-}
-
-int main(int argc, char const *argv[])
-{
+    strcpy(gamingConsole.itemName, "Playstation 5");
+    gamingConsole.quantity = 500;
+    gamingConsole.price = 599.0;
     
+    printf("Console Name: %s\n", gamingConsole.itemName);
+    printf("Consoles in stock: %d\n", gamingConsole.quantity);
+    printf("Price of the gaming console: %.1f$\n", gamingConsole.price);
+
     return 0;
 }
