5303fdc7c7fc16e51d13e1ef736d13e05482bccd
Nomaan JamalUddin <61901753+NONAN23x@users.noreply.github.com>
2022-10-15 07:31:37 +0530

    Add files via upload

---
+++ CallByValue.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int swap(int *a, int *b)
+{
+    printf("--> Swapping\n");
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+    printf("--> a=%d and b=%d\n", *a, *b);
+    printf("--> Swapped!\n");
+}
+
+int main()
+{
+    int a = 3;
+    int b = 5;
+
+    printf("Values of a and b before swapping are: a=%d and b=%d\n", a, b);
+    swap(&a, &b);       //Call by reference
+    printf("Values of a and b after swapping are: a=%d and b=%d\n", a, b);
+    return 0;
+}
--- calculator.c
+++ calculator.c
@@ -1,48 +1,98 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <math.h>
 
-int addition(int x, int y);
-int subtraction(int x, int y);
-int multiplication(int x, int y);
-int divide(int x, int y);
-
-int main(){
-    int x = 0;
-    int y = 0;
-    printf("Please input the two integers seperated by a space.\n");
-    scanf("%d %d", &x, &y);
-    addition(x, y);
-    subtraction(x, y);
-    multiplication(x, y);
-    divide(x, y);
+long add(long a, long b)
+{
+    return a + b;
 }
 
-int addition(int x, int y){
-    int sum = x + y;
-    printf("The sum for %d and %d is %d.\n", x, y, sum);
+long multiply(long a, long b)
+{
+    return a * b;
 }
 
-int subtraction(int x, int y){
-    int sub = 0;
-    if (x > y)
-    sub = x - y;
-    else
-    sub = y - x;
-    printf("The subtraction for %d and %d is %d.\n", x, y, sub);
+long divide(long a, long b)
+{
+    
+    return a / b;
 }
 
-int multiplication(int x, int y){
-    int product = x * y;
-    printf("The product of %d times %d is %d.\n", x, y, product);
+long subtract(long a, long b)
+{
+    return a - b;
 }
 
-int divide(int x, int y){
-    int div = 0;
-    if (x == 0 || y == 0)
-    printf("Cannot divide by zero currently.");
-    else {
-        div = x / y;
-        printf("The division of %d and %d is %d.\n", x, y, div);
+long power(long a, long b)
+{
+    return pow(a, b);
+}
+
+int main(int argc, char const *argv[])
+{
+    int temp1 = 0;
+    int temp2 = 0;
+    int userChoice = 0;
+    
+    
+    while (1)
+    {
+        printf("\n Calculator\n --------\n Supported Operations:\n");
+        printf(" 1) Add\n 2) Multiply\n 3) Divide\n 4) Subtract\n 5) Power\n 6) Exit\n");
+        printf(" $ ");
+        scanf("%d", &userChoice);
+        switch (userChoice)
+        {
+            case 0:
+                printf("ERROR");
+                break;
+
+            case 1:
+                scanf("%ld %ld", &temp1, &temp2);
+                printf("The result is %d", add(temp1, temp2));
+                system("clear");
+                break;
+            
+            case 2:
+                scanf("%ld %ld", &temp1, &temp2);
+                printf("The result is %d", multiply(temp1, temp2));
+                system("clear");
+                break;
+
+            case 3:
+                scanf("%ld %ld", &temp1, &temp2);
+                if (temp2 == 0)
+                {
+                    printf("\nZero Division Error\n");
+                    break;
+                }
+                printf("The result is %d", divide(temp1, temp2));
+                system("clear");
+                break;    
+
+            case 4:
+                scanf("%ld %ld", &temp1, &temp2);
+                printf("The result is %d", subtract(temp1, temp2));
+                system("clear");
+                break;    
+            
+            case 5:
+                scanf("%ld %ld", &temp1, &temp2);
+                printf("The result is %d", power(temp1, temp2));
+                system("clear");
+                break;
+
+            case 6:
+                printf("\nEXITING\n");
+                exit(0);
+                break;
+        
+            default:
+                printf("Invalid Input\n");
+                break;
+        }
     }
     
+    return 0;
 }
